71e98a875beec1ea621cbacdb12ce44f53b56b30
Sovan Patnayak <sovanpatnayak@gmail.com>
2023-05-09 02:30:29 -0400

    Added new project and optimized layout for mobile applications

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Slack from "../assets/slack-bot.png"
 import Editor from "../assets/text-editor.png"
+import SpotifyPlaylist from "../assets/spotify-playlist-generator.png"
 
 const Work = () => {
   return (
@@ -46,7 +47,20 @@ const Work = () => {
                   rel="noopener noreferrer"
                   className="text-[#8892b0] hover:text-[#64ffda] mr-2 h-6 w-6"
                 >
-                  {/* GitHub icon */}
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    role="img"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="feather feather-github"
+                  >
+                    <title>GitHub</title>
+                    <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
+                  </svg>
                 </a>
               </div>
             </div>
@@ -77,7 +91,20 @@ const Work = () => {
                   rel="noopener noreferrer"
                   className="text-[#8892b0] hover:text-[#64ffda] h-6 w-6"
                 >
-                  {/* GitHub icon */}
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    role="img"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="feather feather-github"
+                  >
+                    <title>GitHub</title>
+                    <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
+                  </svg>
                 </a>
               </div>
             </div>
@@ -88,6 +115,60 @@ const Work = () => {
               alt=""
               className="rounded-md shadow w-full h-auto"
             ></img>
+          </div>
+        </div>
+        {/* Project Item End */}
+        {/* Project Item Start */}
+        <div className="flex flex-col sm:flex-row items-center mb-6 sm:mb-16">
+          <div className="w-full sm:w-3/5 mb-4 sm:mb-0">
+            <img
+              src={SpotifyPlaylist}
+              alt=""
+              className="rounded-md shadow w-full h-auto"
+            ></img>
+          </div>
+          <div className="w-full sm:w-2/5 sm:pl-8">
+            <div className="text-right mb-4">
+              <p className="text-[#64ffda] text-xl">Featured Project</p>
+              <h4 className="text-[#ccd6f6] text-3xl font-bold">
+                Spotify Playlist Generator
+              </h4>
+            </div>
+            <div className="bg-[#112240] rounded-md p-6 shadow-xl mb-6">
+              <p className="text-[#8892b0] leading-relaxed">
+                A simple Python application that leverages the Spotify Web API
+                to generate custom playlists using seed tracks from a user's
+                listening history.
+              </p>
+            </div>
+            <div className="text-right">
+              <div className="flex justify-end mb-6">
+                <span className="text-[#8892b0] mr-4">Python</span>
+              </div>
+              <div className="flex justify-end">
+                <a
+                  href="https://github.com/sovanp/spotify-playlist-generator"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-[#8892b0] hover:text-[#64ffda] mr-2 h-6 w-6"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    role="img"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="feather feather-github"
+                  >
+                    <title>GitHub</title>
+                    <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
+                  </svg>
+                </a>
+              </div>
+            </div>
           </div>
         </div>
         {/* Project Item End */}
